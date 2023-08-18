2f690768ee6f4847c2d7ae6c797d828e4c9f7118
mutesihope <hopemutesi250@gmail.com>
2023-08-18 22:33:42 +0200

    Prepared webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: "development",
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+       new HtmlWebpackPlugin({
+              template: './src/index.html',
+               }),
+             ],
+                output: {
+                         filename: '[name].bundle.js',
+                             path: path.resolve(__dirname, 'dist'),
+                               },
+                               module: {
+                                rules: [
+                                  {
+                                    test: /\.css$/i,
+                                    use: ["style-loader", "css-loader"],
+                                  },
+                                ],
+                              },
+};
